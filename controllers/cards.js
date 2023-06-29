37760de89bb2b1e76208f152885c633173b0d81b
Artem Khomutinnikov <89296212+wellplayed-pro@users.noreply.github.com>
2023-06-29 03:19:38 +0300

    save awp

--- controllers/cards.js
+++ controllers/cards.js
@@ -60,7 +60,7 @@ const deleteLikeCard = (req, res) => {
     .catch((error) => {
       if (error.name === 'CastError') {
         res.status(ERROR_VALIDATION).send({ message: 'Переданы некорректные данные' });
-      } else if (error.name === 'Not Found') {
+      } else if (error.message === 'Not Found') {
         res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
       } else {
         res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', error: error.message });
