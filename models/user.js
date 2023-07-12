3f0204901acdef9ac1db426b14ff6be75ae94f8e
Artem Khomutinnikov <89296212+wellplayed-pro@users.noreply.github.com>
2023-07-12 23:30:46 +0300

    fix review

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     required: true,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Введите правильную ссылку',
+    },
   },
   about: {
     type: String,
